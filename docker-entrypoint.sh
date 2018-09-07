755d65d0eec439fff7ceb3e0e564579f06085b63
Andrei Duhnea <andrei@duhnea.net>
2018-09-07 18:12:31 +0300

    [refs #97321] Docker & Rancher config updates

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,14 +1,20 @@
 #!/bin/sh
 
+if [ -z "$CHECK_NAME" ]; then
+  echo "CHECK_NAME is undefined"
+  exit 1
+fi
+
 if [ -z "$AVAILABILITY_CHECK_INTERVAL" ]; then
   export AVAILABILITY_CHECK_INTERVAL=300
 fi
 
+
 case "$1" in
     run)
-        exec python monitoring/availability.py \
-            --endpoints-csv data/endpoints.csv \
-            --output data/$(date +%Y%m%d_%H%M%S)_availability.csv \
+        exec python monitoring/${CHECK_NAME}.py \
+            --endpoints-csv data/monitoring_targets.csv \
+            --output out/${CHECK_NAME}_$(date +%Y%m%d_%H%M%S).csv \
             --check-interval ${AVAILABILITY_CHECK_INTERVAL}
         ;;
     *)
